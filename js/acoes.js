9fdad2acc63b4f52fcca1a6c786f26cbd44298c2
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2024-03-18 17:30:09 -0300

    data 18/03

---
+++ js/acoes.js
@@ -0,0 +1,4 @@
+// alert('teste')
+'use strict' // Começa a usar o código a partir desse comando
+
+document.getElementById('caixa').innerHTML = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
