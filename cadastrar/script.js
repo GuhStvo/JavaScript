943a1b012df74d48cf7978cf2f6b74176fd80619
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2024-03-19 17:07:20 -0300

    Formulário de colaborador

---
+++ cadastrar/script.js
@@ -0,0 +1,11 @@
+function salvar() {
+    let nome = document.getElementById('nome').value
+    let idade = document.getElementById('idade').value
+    let cargo = document.getElementById('cargo').value
+    let salario = document.getElementById('salario').value
+
+    document.getElementById('printNome').innerHTML = nome
+    document.getElementById('printIdade').innerHTML = idade
+    document.getElementById('printCargo').innerHTML = cargo
+    document.getElementById('printSalario').innerHTML = salario
+}
